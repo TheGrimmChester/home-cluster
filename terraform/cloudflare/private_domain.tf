474d00617b045696c9d773769eb0e40f4a8a4c01
Nicolas MELONI <nicolas.meloni@synolia.com>
2023-06-01 08:27:57 +0200

    fix: use ingress subdomain instead of ipv4 to better reflect usage

--- terraform/cloudflare/private_domain.tf
+++ terraform/cloudflare/private_domain.tf
@@ -68,7 +68,7 @@ resource "cloudflare_record" "direct" {
 resource "cloudflare_record" "root" {
   name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  value   = "ingress.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = false
   type    = "CNAME"
   ttl     = 1
